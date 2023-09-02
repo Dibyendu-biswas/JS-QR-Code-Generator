adf76aa90671c9e6dc40a1fd23cd71e58367ae8d
Dibyendu Biswas <117398147+Dibyendu-biswas@users.noreply.github.com>
2023-09-02 22:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const qrBox = document.getElementById("qrBox")
 btn.addEventListener("click",()=>{
    if(inputText.value.length > 0 ){
     qrImage.src=" https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example" + inputText.value
-    console.log("clicked");
     qrBox.classList.add("showItem")
    }else{
     alert("please Enter some Text")
